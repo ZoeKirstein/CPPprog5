1fcd31c7b359bf9b6990ba9d4353fee3abb8b15d
ZoeKirstein <150391867+ZoeKirstein@users.noreply.github.com>
2024-01-15 15:32:53 -0600

    CPPprog5 initial commit
    declare two integers, read in values for int1-int2, if value of integer 1 is greater than integer 2, print the value of integer 1, otherwise print the value of integer 2

---
+++ prog5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int int1;
+    int int2;
+
+    cin >> int1;
+    cin >> int2;
+
+    if(int1>int2){
+        cout<< int1<< endl;
+    }
+    else {
+        cout << int2 << endl;
+    }
+}
